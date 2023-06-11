fd6a85f4fb55f5fcd781c71778423146cd462bcc
hoanmai06 <m.a.i.h.u.u.h.o.a.n@gmail.com>
2023-06-11 17:35:52 +0700

    feat: Added code to alternate table row color

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,21 @@
+import GUI.GUI11;
 import com.formdev.flatlaf.FlatLightLaf;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class Main {
     public static void main(String[] args) {
-//        try {
-//            UIManager.setLookAndFeel(new FlatLightLaf());
-////            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
-//        } catch (UnsupportedLookAndFeelException e) {
-//            throw new RuntimeException(e);
-//        }
+        try {
+            UIManager.setLookAndFeel(new FlatLightLaf());
+//            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        } catch (UnsupportedLookAndFeelException e) {
+            throw new RuntimeException(e);
+        }
+
+        UIDefaults defaults = UIManager.getLookAndFeelDefaults();
+        if (defaults.get("Table.alternateRowColor") == null)
+            defaults.put("Table.alternateRowColor", new Color(240, 240, 240));
 
         GUI11 gui11 = new GUI11(1024, 768);
     }
