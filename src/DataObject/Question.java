0758c3717f35c5500e690e5a1cc68a8e9e3c65c7
hoanmai06 <m.a.i.h.u.u.h.o.a.n@gmail.com>
2023-06-11 23:55:38 +0700

    Added getter and setter for Question

--- src/DataObject/Question.java
+++ src/DataObject/Question.java
@@ -3,9 +3,37 @@ package DataObject;
 import java.util.ArrayList;
 
 public class Question {
-    private String question_name;
-    private String question_text;
-    private int default_mark;
-    private ArrayList<Choice> choices = new ArrayList<Choice>();
+    private String questionName;
+    private String questionText;
+    private int defaultMark;
+    private final ArrayList<Choice> choices = new ArrayList<Choice>();
+
+    public String getQuestionText() {
+        return questionText;
+    }
+
+    public String getQuestionName() {
+        return questionName;
+    }
+
+    public int getDefaultMark() {
+        return defaultMark;
+    }
+
+    public ArrayList<Choice> getChoices() {
+        return choices;
+    }
+
+    public void setQuestionText(String questionText) {
+        this.questionText = questionText;
+    }
+
+    public void setQuestionName(String questionName) {
+        this.questionName = questionName;
+    }
+
+    public void setDefaultMark(int defaultMark) {
+        this.defaultMark = defaultMark;
+    }
 
 }
