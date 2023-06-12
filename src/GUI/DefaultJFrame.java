6842c68772a5a1ad9dfe1eebb3a63187933d0fd8
hoanmai06 <m.a.i.h.u.u.h.o.a.n@gmail.com>
2023-06-12 15:41:57 +0700

    Added listener to save data on closing

--- src/GUI/DefaultJFrame.java
+++ src/GUI/DefaultJFrame.java
@@ -1,7 +1,11 @@
 package GUI;
 
+import DataObject.Singleton;
+
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 
 public class DefaultJFrame extends JFrame {
     public DefaultJFrame(int width, int height) {
@@ -9,5 +13,12 @@ public class DefaultJFrame extends JFrame {
         setSize(new Dimension(width, height));
         setLocationRelativeTo(null);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
+
+        this.addWindowListener(new WindowAdapter(){
+            public void windowClosing(WindowEvent e){
+                Singleton.getInstance().writeCategoriesToFile();
+                System.exit(0);
+            }
+        });
     }
 }
