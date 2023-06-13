bae2328e577058cd7efdd15244941229073eac6d
hoanmai06 <m.a.i.h.u.u.h.o.a.n@gmail.com>
2023-06-13 21:47:09 +0700

    Moved edit Action and ButtonColumn declaration to GUI21

--- src/GUI/QuestionTable.java
+++ src/GUI/QuestionTable.java
@@ -11,19 +11,6 @@ public class QuestionTable extends JTable {
     public QuestionTable(Category category) {
         super(new QuestionTableModel(category));
 
-        Action edit = new AbstractAction("Edit") {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                JTable table = (JTable) e.getSource();
-                int row = Integer.parseInt(e.getActionCommand());
-//                DefaultTableModel model = (DefaultTableModel) table.getModel();
-                JOptionPane.showMessageDialog(null, "Editing row " + row);
-            }
-        };
-
-//        ButtonColumn inc =
-        new ButtonColumn(this, edit, 2);
-
         this.setRowHeight(35);
         this.getColumnModel().getColumn(0).setMaxWidth(25);
         this.getColumnModel().getColumn(2).setMaxWidth(60);
