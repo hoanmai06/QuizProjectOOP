1a4baf1b7ef185c55bde710ca7663d45aac39353
hoanmai06 <m.a.i.h.u.u.h.o.a.n@gmail.com>
2023-07-06 21:06:57 +0700

    Refactored to inherit QuestionListContainer

--- src/DataObject/Category.java
+++ src/DataObject/Category.java
@@ -1,24 +1,14 @@
 package DataObject;
 
 import java.io.Serializable;
-import java.util.ArrayList;
 
-public class Category implements Serializable {
+public class Category extends QuestionListContainer implements Serializable {
     private String categoryName;
     private String categoryInfo;
     private String idNumber;
-    private ArrayList<Question> questions = new ArrayList<Question>();
-
-    public void addQuestion(Question question) {
-        questions.add(question);
-    }
 
     public String getCategoryName() {
         return categoryName;
-    }
-
-    public ArrayList<Question> getQuestions() {
-        return questions;
     }
 
     public void setCategoryName(String categoryName) {
@@ -31,15 +21,5 @@ public class Category implements Serializable {
 
     public void setIdNumber(String id_number) {
         this.idNumber = id_number;
-    }
-
-    public Object[][] getQuestionTableData() {
-        Object[][] questionTaleData = new Object[questions.size()][3];
-
-        for (int i = 0; i < questions.size(); i++) {
-            questionTaleData[i] = questions.get(i).getQuestionTableRow();
-        }
-
-        return questionTaleData;
     }
 }
