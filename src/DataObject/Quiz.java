48bc558f06056d1e8e59c828f802edc54971cad3
hoanmai06 <m.a.i.h.u.u.h.o.a.n@gmail.com>
2023-07-06 21:05:12 +0700

    Added more field and now inherits QuestionListContainer

--- src/DataObject/Quiz.java
+++ src/DataObject/Quiz.java
@@ -1,10 +1,29 @@
 package DataObject;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 
-public class Quiz implements Serializable {
+public class Quiz extends QuestionListContainer implements Serializable {
+    public static final int TIME_TYPE_HOUR = 1;
+    public static final int TIME_TYPE_MINUTE = 0;
     private String name;
     private String description;
+    private int timeLimit = -1;
+    private int timeLimitType = Quiz.TIME_TYPE_HOUR;
+    private ArrayList<String> previousAttemptList = new ArrayList<>();
+
+    public Object[][] getAttemptTableData() {
+        if (previousAttemptList.size() == 0)
+            return new Object[][]{{"None", ""}};
+
+        Object[][] attemptTableData = new Object[previousAttemptList.size()][2];
+        for (int i = 0; i < previousAttemptList.size(); i++) {
+            attemptTableData[i][0] = "Preview";
+            attemptTableData[i][1] = previousAttemptList.get(i);
+        }
+
+        return attemptTableData;
+    }
 
     public String getName() {
         return name;
@@ -20,5 +39,25 @@ public class Quiz implements Serializable {
 
     public void setDescription(String description) {
         this.description = description;
+    }
+
+    public int getTimeLimit() {
+        return timeLimit;
+    }
+
+    public void setTimeLimit(int timeLimit) {
+        this.timeLimit = timeLimit;
+    }
+
+    public int getTimeLimitType() {
+        return timeLimitType;
+    }
+
+    public void setTimeLimitType(int timeLimitType) {
+        this.timeLimitType = timeLimitType;
+    }
+
+    public ArrayList<String> getPreviousAttemptList() {
+        return previousAttemptList;
     }
 }
