85c445814e4aa689e1ed6d310993f85011ddea22
hoanmai06 <m.a.i.h.u.u.h.o.a.n@gmail.com>
2023-07-06 21:05:23 +0700

    Added new GUI

---
+++ src/GUI/GUI63.java
@@ -0,0 +1,85 @@
+package GUI;
+
+import DataObject.CategoriesSingleton;
+import DataObject.Category;
+import DataObject.Question;
+import com.formdev.flatlaf.FlatLightLaf;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class GUI63 extends DefaultJFrame {
+    private JPanel TopBar;
+    private JPanel MidZone1Container;
+    private JPanel MidZone1;
+    private JPanel MidZone2Container;
+    private JPanel MidZone2;
+    private JCheckBox alsoShowQuestionsFromCheckBox;
+    private JCheckBox alsoShowOldQuestionCheckBox;
+    private JComboBox categoryComboBox;
+    private JTable questionTable;
+    private JPanel guiPanel;
+    private JButton ADDSELECTEDQUESTIONTOButton;
+
+    public GUI63(int width, int height) {
+        super(width, height);
+        setContentPane(guiPanel);
+        setVisible(true);
+
+        categoryComboBox.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                int selectedIndex = categoryComboBox.getSelectedIndex();
+                Category selectedCategory = CategoriesSingleton.getInstance().getCategories().get(selectedIndex);
+
+                QuestionTableModel questionTableModel = (QuestionTableModel) questionTable.getModel();
+                questionTableModel.setRowCount(0);
+                for (Question question : selectedCategory.getQuestions()) {
+                    questionTableModel.addRow(question.getQuestionTableRow());
+                }
+            }
+        });
+
+        Action edit = new AbstractAction("Edit") {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                int row = Integer.parseInt(e.getActionCommand());
+                dispose();
+                new GUI32(getWidth(), getHeight(), categoryComboBox.getSelectedIndex(), row);
+            }
+        };
+
+        new EditButtonColumn(questionTable, edit, 2);
+    }
+
+    public static void main(String[] args) {
+        try {
+            UIManager.setLookAndFeel(new FlatLightLaf());
+        } catch (UnsupportedLookAndFeelException e) {
+            throw new RuntimeException(e);
+        }
+
+        UIDefaults defaults = UIManager.getLookAndFeelDefaults();
+        if (defaults.get("Table.alternateRowColor") == null)
+            defaults.put("Table.alternateRowColor", new Color(240, 240, 240));
+
+        SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                new GUI63(1024, 768);
+            }
+        });
+    }
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+
+        //categoryComboBox
+        categoryComboBox = new JComboBox(CategoriesSingleton.getInstance().getCategoryNameList());
+
+        // questionTable
+        questionTable = new QuestionTable(CategoriesSingleton.getInstance().getCategories().get(0));
+
+    }
+}
