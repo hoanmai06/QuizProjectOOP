242fc52698de74f0fa740a7d59cd504764e5a6d7
hoanmai06 <m.a.i.h.u.u.h.o.a.n@gmail.com>
2023-07-09 17:14:29 +0700

    Minor changes

--- src/GUIs/GUI65.java
+++ src/GUIs/GUI65.java
@@ -28,6 +28,7 @@ public class GUI65 extends DefaultJFrame {
     private JPanel MidZone2;
     private JCheckBox includeQuestionsFromSubcategoriesCheckBox;
     private JComboBox categoryComboBox;
+    private JComboBox randomQuestionComboBox;
     private JTable questionTable;
     private JButton ADDRANDOMQUESTIONTOButton;
     private JTabbedPane tabbedPane1;
