35b3ada2b856aa5fccf46c0822dfa043e4ced919
hoanmai06 <m.a.i.h.u.u.h.o.a.n@gmail.com>
2023-07-10 22:52:47 +0700

    Thêm cái sửa ký tự đặc biệt của HTML

--- src/GUIs/QuestionPanelManager.java
+++ src/GUIs/QuestionPanelManager.java
@@ -19,12 +19,22 @@ public class QuestionPanelManager {
     private JPanel questionPanel;
     private JLabel questionText;
     private JButton clearSelectionButton;
+    private JPanel answerPanel;
+    private JLabel answerLabel;
     JRadioButton[] choiceRadioButtonList;
 
     public QuestionPanelManager(int index, Question question, JCheckBox navigationCheckBox) {
         // Customize JLabel
         questionIndex.setText(String.valueOf(index));
-        questionText.setText("<html>%s</html>".formatted(question.getText()));
+        questionText.setText(
+                "<html>%s</html>".formatted(
+                        formatHTMLSafe(question.getText())
+                ));
+
+        answerLabel.setText(
+                "<html>The correct answer is: %s</html>".formatted(
+                        formatHTMLSafe(question.getAnswer().getText())
+                ));
 
         // Setup choice panel
         int numberOfChoices = question.getChoices().size();
@@ -41,7 +51,7 @@ public class QuestionPanelManager {
         };
 
         for (int i = 0; i < numberOfChoices; i++) {
-            choiceRadioButtonList[i] = new JRadioButton("<html>" + question.getChoices().get(i).getText() + "</html>");
+            choiceRadioButtonList[i] = new JRadioButton("<html>" + formatHTMLSafe(question.getChoices().get(i).getText()) + "</html>");
 
             choiceRadioButtonList[i].addActionListener(radioButtonListener);
 
@@ -56,6 +66,39 @@ public class QuestionPanelManager {
                 navigationCheckBox.setSelected(false);
             }
         });
+    }
+    
+    public String formatHTMLSafe(String str) {
+        String newstr="";
+
+        for(int i=0;i<str.length();i++)
+            if(str.charAt(i)=='\'')
+                newstr = newstr + "&#39;";
+            else if (str.charAt(i)=='\"')
+                newstr = newstr + "&#34;";
+            else if(str.charAt(i)=='&')
+                newstr = newstr + "&#38;";
+            else if(str.charAt(i)=='<')
+                newstr = newstr + "&#60;";
+            else if(str.charAt(i)=='>')
+                newstr = newstr + "&#62;";
+            else
+                newstr= newstr + String.copyValueOf(str.toCharArray(),i,1);
+
+            return newstr;
+    }
+
+    public void disableButton() {
+        for (JRadioButton radioButton : choiceRadioButtonList) {
+            radioButton.setEnabled(false);
+        }
+
+        clearSelectionButton.setEnabled(false);
+        clearSelectionButton.setVisible(false);
+    }
+
+    public void showAnswer() {
+        answerPanel.setVisible(true);
     }
 
     public JPanel getQuestionPanel() {
