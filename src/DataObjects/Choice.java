40950a10abd91bf7e8327418c3c031c6e55a4b1d
nguyentuandung <nguyentunadung23012003@gmail.com>
2023-07-10 20:55:21 +0700

    setchoice and setquestion

--- src/DataObjects/Choice.java
+++ src/DataObjects/Choice.java
@@ -17,7 +17,23 @@ public class Choice implements Serializable {
     public String getChoiceName() { return choiceName;}
 
     public void setChoiceText(String choiceText) {
-        this.choiceText = choiceText;
+        String newstr="";
+        for(int i=0;i<choiceText.length();i++)
+            if(choiceText.charAt(i)=='\'')
+                newstr = newstr + "&#39;";
+            else if (choiceText.charAt(i)=='\"')
+                newstr = newstr + "&#34;";
+            else if(choiceText.charAt(i)=='&')
+                newstr = newstr + "&#38;";
+            else if(choiceText.charAt(i)=='<')
+                newstr = newstr + "&#60;";
+            else if(choiceText.charAt(i)=='>')
+                newstr = newstr + "&#39;";
+            else
+                newstr= newstr + String.copyValueOf(choiceText.toCharArray(),i,1);
+
+        this.choiceText = newstr;
+
     }
 
     public void setGrade(int grade) {
