d8f5711d36d478928f2d91321ba0ae287c0008fa
TrungNgT <trungnguyenthanh.eworking@gmail.com>
2023-07-12 05:25:30 +0700

    sua loi dong tieu de khong ngan cach upadate kha nang luu tru thong tin anh tuong ung voi dong line truoc no, tu do cap nhat thuoc tinh cho question, choice

--- src/Algorithms/read_docx.java
+++ src/Algorithms/read_docx.java
@@ -5,10 +5,12 @@ import DataObjects.Question;
 import org.apache.poi.openxml4j.opc.OPCPackage;
 import org.apache.poi.xwpf.usermodel.XWPFDocument;
 import org.apache.poi.xwpf.usermodel.XWPFParagraph;
+import org.apache.poi.xwpf.usermodel.XWPFPictureData;
 
 import javax.swing.*;
 import java.io.FileInputStream;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class read_docx {
@@ -20,86 +22,122 @@ public class read_docx {
     public ArrayList<Question> readFromDOCX(String absPath) {
 
         ArrayList<Question> listQ = new ArrayList<>();
+        ArrayList<String> typeOfLine = new ArrayList<>();
+        ArrayList<byte[]> bytes = new ArrayList<>();
 
         try{
             FileInputStream fis = new FileInputStream(absPath);
             XWPFDocument document = new XWPFDocument(OPCPackage.open(fis));
             List<XWPFParagraph> paragraphList =  document.getParagraphs();
 
+            List<XWPFPictureData> piclist = document.getAllPictures();
+            for (XWPFPictureData picture : piclist) {
+                bytes.add(picture.getData());
+            }
+
+            for (int i=0; i<paragraphList.size(); i++) {
+                if (paragraphList.get(i).isEmpty()) typeOfLine.add("blank_space");
+                else {
+                    if (!paragraphList.get(i).getText().equals(""))  typeOfLine.add("text");
+                    else typeOfLine.add("images");
+                }
+            }
+
+            int indexOfPic = 0;
             int indexOfLine = 0;
+
             String beforeString = new String("");
             String line = new String();
             Question q = new Question();
+            Choice c = new Choice();
             ArrayList<Choice> listC = new ArrayList<>();
             List<String> listCN = new ArrayList<>();
 
             while (indexOfLine < paragraphList.size()) {
                     line = paragraphList.get(indexOfLine).getText();
-                    indexOfLine ++;if(line == null) break;
+                    if(line == null) break;
                 if(line.trim().equals("")) {
                     if(beforeString.trim().equals("ANS") || beforeString.trim().equals(""))
                         beforeString = "";
-                    if(beforeString.trim().equals("text")){
+                    if(beforeString.trim().equals("text") && typeOfLine.get(indexOfLine).equals("blank_space")){
                         showError(indexOfLine);
                         listQ.clear();
                         break;
+                    }
+                    if(typeOfLine.get(indexOfLine).equals("images")) {
+                        if(typeOfLine.get(indexOfLine-1).equals("ques")) {
+                            q.setImageData(bytes.get(indexOfPic));
+                        }
+                        if(typeOfLine.get(indexOfLine-1).equals("choice")) {
+                            c.setc_ImageData(bytes.get(indexOfPic));
+                        }
+                        indexOfPic++;
                     }
                 }
                 else {
-                    if(beforeString.trim().equals("")) {
-                        String txtQ[] = line.split("\\.\\s", 2);
-                        q = new Question();
-                        q.setName(txtQ[0]);
-                        q.setText(txtQ[1]);
-                        beforeString = "text";
-                        listC = new ArrayList<>();
-                        continue;
-                    }
-                    if(beforeString.trim().equals("ANS")) {
-                        showError(indexOfLine);
-                        listQ.clear();
-                        break;
-                    }
-                    if(beforeString.trim().equals("text")) {
-                        String txtCorA[] = line.split("\\s", 2);
-                        if(!txtCorA[0].equals("ANSWER:")) {
-                            if(txtCorA[0].length()==2 && txtCorA[0].charAt(1)=='.' && txtCorA[0].charAt(0)>='A' && txtCorA[0].charAt(0)<='Z' && txtCorA[1].charAt(0)!=' ') {
-                                Choice c = new Choice();
-                                c.setName(txtCorA[0]);
-                                c.setText(txtCorA[1]);
-                                listC.add(c);
-                                listCN.add(txtCorA[0]);
-                            }
-                            else {
+                    if (typeOfLine.get(indexOfLine).equals("text")) {
+                        if (beforeString.trim().equals("")) {
+                            typeOfLine.set(indexOfLine, "ques");
+                            String txtQ[] = line.split("\\.\\s", 2);
+                            if(Arrays.stream(txtQ).count() < 2) {
                                 showError(indexOfLine);
                                 listQ.clear();
                                 break;
+                            } else {
+                                q = new Question();
+                                q.setName(txtQ[0]);
+                                q.setText(txtQ[1]);
+                                beforeString = "text";
+                                listC = new ArrayList<>();
+                                continue;
                             }
                         }
-                        else {
-                            if(listCN.contains("A.") && listCN.contains("B.") && listCN.contains(txtCorA[1] + ".")) {
-                                String ans = txtCorA[1] + ".";
-                                //# setGrade cho tung choice, chu y doc file thi cau hoi chi co 1 cau tra loi
-                                for(Choice c : listC) {
-                                    if ((c.getName()).equals(ans)) {
-                                        c.setGrade(1);
-                                        q.setAnswer(c);
-                                    }
-                                    else c.setGrade(0);
+                        if (beforeString.trim().equals("ANS")) {
+                            showError(indexOfLine);
+                            listQ.clear();
+                            break;
+                        }
+                        if (beforeString.trim().equals("text")) {
+                            String txtCorA[] = line.split("\\s", 2);
+                            if (!txtCorA[0].equals("ANSWER:")) {
+                                if (txtCorA[0].length() == 2 && txtCorA[0].charAt(1) == '.' && txtCorA[0].charAt(0) >= 'A' && txtCorA[0].charAt(0) <= 'Z' && txtCorA[1].charAt(0) != ' ') {
+                                    typeOfLine.set(indexOfLine, "choice");
+                                    c = new Choice();
+                                    c.setName(txtCorA[0]);
+                                    c.setText(txtCorA[1]);
+                                    listC.add(c);
+                                    listCN.add(txtCorA[0]);
+                                } else {
+                                    showError(indexOfLine);
+                                    listQ.clear();
+                                    break;
                                 }
-                                q.setChoices(listC);
-                                listCN.clear();
-                                listQ.add(q);
-                                beforeString = "ANS";
-                            }
-                            else {
-                                showError(indexOfLine);
-                                listQ.clear();
-                                break;
+                            } else {
+                                if (listCN.contains("A.") && listCN.contains("B.") && listCN.contains(txtCorA[1] + ".")) {
+                                    typeOfLine.set(indexOfLine, "answer");
+                                    String ans = txtCorA[1] + ".";
+                                    //# setGrade cho tung choice, chu y doc file thi cau hoi chi co 1 cau tra loi
+                                    for (Choice c1 : listC) {
+                                        if ((c1.getName()).equals(ans)) {
+                                            c1.setGrade(1);
+                                            q.setAnswer(c1);
+                                        } else c.setGrade(0);
+                                    }
+                                    q.setChoices(listC);
+                                    listCN.clear();
+                                    listQ.add(q);
+                                    beforeString = "ANS";
+                                } else {
+                                    showError(indexOfLine);
+                                    listQ.clear();
+                                    break;
+                                }
                             }
                         }
                     }
                 }
+                indexOfLine ++;
+
             }
             document.close();
 
@@ -109,15 +147,28 @@ public class read_docx {
         return listQ;
     }
 
-//    public static void main(String[] args) {
+//    public static void main(String[] args) throws IOException {
+//        BufferedImage img = null;
 //        read_docx r = new read_docx();
-//        ArrayList<Question> list = r.readFromDOCX("C:\\Users\\ADMIN\\OneDrive - Hanoi University of Science and Technology\\Desktop\\demo_read_from_file.docx");
-//        if(list.size() > 0) {
-//            JFrame Sframe = new JFrame("SuccessMessage");
-//            JOptionPane.showMessageDialog(Sframe, "Success "+list.size() +" questions", "Valid input", JOptionPane.INFORMATION_MESSAGE);
-//            for(Question o : list) {
-//                System.out.println(o.getAnswer());
-//            }
-//        }
+//        ArrayList<Question> list = r.readFromDOCX("C:\\Users\\ADMIN\\QuizProjectOOP\\src\\Test_cases\\docx_has_image.docx");
+////        if(list.size() > 0) {
+////            JFrame Sframe = new JFrame("SuccessMessage");
+////            JOptionPane.showMessageDialog(Sframe, "Success "+list.size() +" questions", "Valid input", JOptionPane.INFORMATION_MESSAGE);
+////            for(Question o : list) {
+////                System.out.println(o.getAnswer());
+////            }
+////        }
+
+    //# TEST IN RA PANEL CHUA IMAGE CUA CAU HOI HOAC CHOICE
+//        ByteArrayInputStream bais = new ByteArrayInputStream(list.get(0).getImageData());
+//        img = ImageIO.read(bais);
+//        ImageIcon image = new ImageIcon(img);
+//        JLabel label = new JLabel();
+//        label.setIcon(image);
+//        JFrame frame = new JFrame("Day la anh tuong ung voi dong: " + list.get(0).getText());
+//        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+//        frame.setSize(500, 500);
+//        frame.setVisible(true);
+//        frame.add(label);
 //    }
 }
