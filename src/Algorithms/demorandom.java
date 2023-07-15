7320b16b873b18f3a9299f95de412d6829a7ff6b
TrungNgT <trungnguyenthanh.eworking@gmail.com>
2023-07-15 07:31:03 +0700

    Biet duoc cach lay question ra tu data center

--- src/Algorithms/demorandom.java
+++ src/Algorithms/demorandom.java
@@ -1,7 +1,12 @@
 package Algorithms;
 
+import DataObjects.CategoriesSingleton;
+import DataObjects.Question;
+import DataObjects.Category;
+import DataObjects.QuestionListContainer;
+
+import java.util.ArrayList;
+
 public class demorandom {
-   public static void main(String[] args) {
-      System.out.print("Hello Java");
-   }
+   ArrayList<Question> ques = CategoriesSingleton.getInstance().getCategories().get(0).getQuestions();
 }
