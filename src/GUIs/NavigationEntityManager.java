e6c2298013a99d3b2861e62875d160e4cb1ac125
hoanmai06 <m.a.i.h.u.u.h.o.a.n@gmail.com>
2023-07-15 15:08:12 +0700

    Thêm file config chứa mấy hằng số tuỳ chỉnh GUI

--- src/GUIs/NavigationEntityManager.java
+++ src/GUIs/NavigationEntityManager.java
@@ -5,10 +5,6 @@ import javax.swing.border.LineBorder;
 import java.awt.*;
 
 public class NavigationEntityManager {
-    public static Color CORRECT = new Color(0x7FFF7F);
-    public static Color INCORRECT = new Color(0xE77070);
-    public static Color SELECTED = Color.GRAY;
-    public static Color NOT_SELECTED = Color.WHITE;
     private JLabel indexLabel;
     private JLabel colorLabel;
     private JPanel navigationEntity;
