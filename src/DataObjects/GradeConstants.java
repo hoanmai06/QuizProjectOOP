96c495a99bc5f35f06777f6a63cacbeb547aa026
hoanmai06 <m.a.i.h.u.u.h.o.a.n@gmail.com>
2023-07-21 13:41:42 +0700

    Added getGrade function

--- src/DataObjects/GradeConstants.java
+++ src/DataObjects/GradeConstants.java
@@ -1,6 +1,6 @@
 package DataObjects;
 
-public interface GradeConstants {
+public class GradeConstants {
     public static final int GRADE_NONE = 0;
     public static final int GRADE_100 = 1;
     public static final int GRADE_90 = 2;
@@ -23,4 +23,30 @@ public interface GradeConstants {
     public static final int GRADE_10 = 17;
     public static final int GRADE_5 = 18;
     public static final int GRADE_MINUS_5 = 19;
+
+    public static double getGrade(int gradeConstant) {
+        return switch (gradeConstant) {
+            case 1 -> 1;
+            case 2 -> 0.9;
+            case 3 -> 5 / 6d;
+            case 4 -> 0.8;
+            case 5 -> 0.75;
+            case 6 -> 0.7;
+            case 7 -> 0.5;
+            case 8 -> 0.4;
+            case 9 -> 1 / 3d;
+            case 10 -> 0.3;
+            case 11 -> 0.25;
+            case 12 -> 0.2;
+            case 13 -> 1 / 6d;
+            case 14 -> 1 / 7d;
+            case 15 -> 0.125;
+            case 16 -> 1 / 9d;
+            case 17 -> 0.1;
+            case 18 -> 0.05;
+            case 19 -> -0.05;
+
+            default -> 0;
+        };
+    }
 }
